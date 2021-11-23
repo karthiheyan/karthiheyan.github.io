9cac02ec40aefec6a55a8cf9c3660cfd6c69333e
karthiheyan <karthiheyan23@gmail.com>
2021-11-24 01:47:08 +0530

    UI & UX Change

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ window.onscroll = () => {
 };
 
 // sticky navbar End
+
+// sidebar Start
+
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e) => {
+    let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+    arrowParent.classList.toggle("showMenu");
+  });
+}
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+// sidebar end
